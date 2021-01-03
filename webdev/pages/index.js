2e6ae6dc91e69ef9f7e8b853ef975b7674aba5f7
Anirudh Krishna <anirudhkrishna@Anirudhs-MacBook-Pro.local>
2021-01-03 14:34:26 -0800

    home page coming together

--- webdev/pages/index.js
+++ webdev/pages/index.js
@@ -10,10 +10,47 @@ export default class Home extends React.Component {
   return (
     <>
         <Nav />
-    <h1>TSA</h1>
-    <footer>
-  <p>TSA</p>
-</footer>
+    <div id="page-container">
+   <div id="content-wrap">
+   </div>
+
+ 
+      <img style={{"float":"left",'width':'70%','paddingLeft':'8%'}} src={"image1.png"} />
+      <div class="content-heading">
+      <h3 style={{'textDecoration':'underline'}}>Header</h3>
+      <p>Here is some text to put heyo madhav how u doing and wasssgoooood prassy how r u doing. I should be doing notes. Ok bye</p>
+      </div>
+
+
+
+   <footer id="footer">
+   <div style={{'backgroundColor':'#C1C8E4'}}>
+<h1>Footer</h1>
+   </div>
+   </footer>
+
+
+ </div>
+
+ <style JSX>{`
+  #page-container {
+  position: relative;
+  min-height: 100vh;
+}
+
+#content-wrap {
+  padding-bottom: 2.5rem; 
+}
+
+#footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;
+}
+
+
+ `}</style>
 
     </>
   )
