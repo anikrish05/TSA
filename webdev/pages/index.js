dbebece5fd4d6d2620d695ed6d9d563f43c97b8d
Anirudh Krishna <anirudhkrishna@Anirudhs-MacBook-Pro.local>
2021-01-02 21:20:31 -0800

    created next app

---
+++ webdev/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+export default class Home extends React.Component {
+  render(){
+  return (
+    <div>
+    <h1>Hi</h1>
+
+    </div>
+  )
+}
+}
