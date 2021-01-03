b728abeea51d51ec6275099fd6382b57d67f6a5e
Anirudh Krishna <anirudhkrishna@Anirudhs-MacBook-Pro.local>
2021-01-03 12:35:18 -0800

    navbar color switched

--- webdev/components/navbar.js
+++ webdev/components/navbar.js
@@ -18,7 +18,7 @@ export default function nav(){
 
     <p style={{'color':'#79C5E7'}} class="text-left">COVID- 19- What You need to know to stay safe</p>
     </div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav style={{'backgroundColor':'#79C5E7'}} class="navbar navbar-expand-lg navbar-light">
   <a class="navbar-brand" href="/">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
