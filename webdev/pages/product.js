51e2c7f18c89993ea96ad480ea1aca5a7873d2b3
Anirudh Krishna <anirudhkrishna@Anirudhs-MacBook-Pro.local>
2021-01-06 23:08:15 -0800

    fixed bugs

--- webdev/pages/product.js
+++ webdev/pages/product.js
@@ -13,8 +13,8 @@ export default class product extends React.Component {
         {/*Madhav start code here*/}
 <img style={{'width':'50%','height':'400px'}} src={'image3.png'} />
 <img style={{'width':'30%','height':'400px', 'float':'right'}} src={'image2.png'} />
-<img style={{'width':'60%','height';'400px'}} src={'plot.jpeg'} />
-<h3 style={{'textAllign':'center'}}>Information About Our Product</h3>
+<img style={{'width':'60%','height':'400px'}} src={'plot.jpeg'} />
+<h3 style={{'textAlign':'center'}}>Information About Our Product</h3>
 <p> Our product helps our customers to talk to doctors online via video call and calling platforms, allowing the patients and the doctors to talk to each other without worrying about social distancing. We include a list of Doctors that are available for meeting patients at any point of time. We also have information about Covid-19 about your area, and includes information including the number of cases, hospitilizations, etc.</p>
 
 
