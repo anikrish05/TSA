58b4e16babafa90f30fa88ccb296dab756855533
unknown <prasup2005@gmail.com>
2021-01-09 14:42:28 -0800

    some message

--- webdev/pages/aboutus.js
+++ webdev/pages/aboutus.js
@@ -12,6 +12,14 @@ export default class about extends React.Component {
         <Nav />
         {/*Prassana start code here*/}
     <h1>About Us</h1>
+    <img style= {{"float": "Left", 'width': '30%'}} src = {"about_us_img_1.png"} />
+    <p>This is where details about who we are goes/ Ill do it later</p>
+    <img style ={{"float": "right", 'marginTop': '19%','width': '30%' , 'marginRight' : '3%'}} src = {"about_us_img_2.png"} />
+    <p>This is about our mission statement/which again I will do later</p>
+    <img style = {{"float": "left",'marginTop': '25%', 'width': '32%',"marginRight" : "35%"}} src = {"about_us_img_3.png"} />
+    <p>This is where we will do our project, I'll try and make a responsive button that takes them to product page</p>
+    
+
     {/*Prassana stop code here*/}
 
 
