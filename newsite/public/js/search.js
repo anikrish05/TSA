707df2240fbe278552cc28328b630531580a9eae
Anirudh Krishna <anirudhkrishna@Anirudhs-MacBook-Pro.local>
2021-01-09 15:28:09 -0800

    search pge

--- newsite/public/js/search.js
+++ newsite/public/js/search.js
@@ -1,8 +1,30 @@
 var urlParams=''
 var myParam=''
+var dataState=''
 $(document).ready(function () {
   $("#navBar").load("/components/navbar");
    $("#footer").load("/components/footer");
     urlParams = new URLSearchParams(window.location.search);
 	 myParam = urlParams.get('id');
+
+	 $.ajax('https://api.covidtracking.com/v1/states/current.json',{
+	 	success:function(data){
+	 		console.log(data)
+	 		    for(var i=0;i<data.length;i++){
+      	if (data[i]['state']==myParam.toUpperCase()){
+           dataState=data[i]
+            $('#data').append(
+            	'<p>Positive cases:'
+            	+dataState['positive']
+            	+'</br>Deaths:'
+            	+dataState['death']
+            	+'</br>State:'
+            	+dataState['state']
+            	+'</br>Negative Cases:'
+            	+dataState['negative']
+            	+'</p>')
+      	}
+    }
+	 	}
+	 });
 });
