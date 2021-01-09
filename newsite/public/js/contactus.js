b809bf414757d146bad6b66c0f2a8f3315dfd0d0
Anirudh Krishna <anirudhkrishna@Anirudhs-MacBook-Pro.local>
2021-01-09 02:04:54 -0800

    finsihed home, contact us, about us, events pages

--- newsite/public/js/contactus.js
+++ newsite/public/js/contactus.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
   $("#navBar").load("/components/navbar");
   $("#footer").load("/components/footer");
+ var map = L.map('mapid').setView([37.773972, -122.431297], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([37.773972, -122.431297]).addTo(map)
+    .bindPopup('Our location')
+    .openPopup();
 });
